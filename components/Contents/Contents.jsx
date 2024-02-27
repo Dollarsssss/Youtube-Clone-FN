1d2bc8d02ce8e54dbc809c1309f2ded0cb9ef429
Dollars <aomsup5185@gmail.com>
2024-02-27 13:44:39 +0700

    Update

--- components/Contents/Contents.jsx
+++ components/Contents/Contents.jsx
@@ -9,7 +9,139 @@ function Contents() {
       <Tags></Tags>
       <div className='container-content'>
         <div className='item-content'>
-      x
+          <div className='img-cover'>
+            <img src='https://via.placeholder.com/400x231' alt="" />
+          </div>
+          <div className='item-info'>
+            <div className='img-channel'>
+              <img src='https://via.placeholder.com/36x36' alt="" />
+            </div>
+            <div className='item-info-text'>
+              <h4 className='text-title'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident eum eos aperiam molestiae excepturi doloremque neque voluptatum error sed quasi.</h4>
+              <p className='text-channel'>King Soyer</p>
+              <p className='text-countWatch'> การดู 1.6 แสน ครั้ง 15 ชั่วโมงที่ผ่านมา</p>
+            </div>
+          </div>
+        </div>
+        <div className='item-content'>
+          <div className='img-cover'>
+            <img src='https://via.placeholder.com/400x231' alt="" />
+          </div>
+          <div className='item-info'>
+            <div className='img-channel'>
+              <img src='https://via.placeholder.com/36x36' alt="" />
+            </div>
+            <div className='item-info-text'>
+              <h4 className='text-title'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident eum eos aperiam molestiae excepturi doloremque neque voluptatum error sed quasi.</h4>
+              <p className='text-channel'>King Soyer</p>
+              <p className='text-countWatch'> การดู 1.6 แสน ครั้ง 15 ชั่วโมงที่ผ่านมา</p>
+            </div>
+          </div>
+        </div>
+        <div className='item-content'>
+          <div className='img-cover'>
+            <img src='https://via.placeholder.com/400x231' alt="" />
+          </div>
+          <div className='item-info'>
+            <div className='img-channel'>
+              <img src='https://via.placeholder.com/36x36' alt="" />
+            </div>
+            <div className='item-info-text'>
+              <h4 className='text-title'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident eum eos aperiam molestiae excepturi doloremque neque voluptatum error sed quasi.</h4>
+              <p className='text-channel'>King Soyer</p>
+              <p className='text-countWatch'> การดู 1.6 แสน ครั้ง 15 ชั่วโมงที่ผ่านมา</p>
+            </div>
+          </div>
+        </div>
+        <div className='item-content'>
+          <div className='img-cover'>
+            <img src='https://via.placeholder.com/400x231' alt="" />
+          </div>
+          <div className='item-info'>
+            <div className='img-channel'>
+              <img src='https://via.placeholder.com/36x36' alt="" />
+            </div>
+            <div className='item-info-text'>
+              <h4 className='text-title'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident eum eos aperiam molestiae excepturi doloremque neque voluptatum error sed quasi.</h4>
+              <p className='text-channel'>King Soyer</p>
+              <p className='text-countWatch'> การดู 1.6 แสน ครั้ง 15 ชั่วโมงที่ผ่านมา</p>
+            </div>
+          </div>
+        </div>
+        <div className='item-content'>
+          <div className='img-cover'>
+            <img src='https://via.placeholder.com/400x231' alt="" />
+          </div>
+          <div className='item-info'>
+            <div className='img-channel'>
+              <img src='https://via.placeholder.com/36x36' alt="" />
+            </div>
+            <div className='item-info-text'>
+              <h4 className='text-title'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident eum eos aperiam molestiae excepturi doloremque neque voluptatum error sed quasi.</h4>
+              <p className='text-channel'>King Soyer</p>
+              <p className='text-countWatch'> การดู 1.6 แสน ครั้ง 15 ชั่วโมงที่ผ่านมา</p>
+            </div>
+          </div>
+        </div>
+        <div className='item-content'>
+          <div className='img-cover'>
+            <img src='https://via.placeholder.com/400x231' alt="" />
+          </div>
+          <div className='item-info'>
+            <div className='img-channel'>
+              <img src='https://via.placeholder.com/36x36' alt="" />
+            </div>
+            <div className='item-info-text'>
+              <h4 className='text-title'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident eum eos aperiam molestiae excepturi doloremque neque voluptatum error sed quasi.</h4>
+              <p className='text-channel'>King Soyer</p>
+              <p className='text-countWatch'> การดู 1.6 แสน ครั้ง 15 ชั่วโมงที่ผ่านมา</p>
+            </div>
+          </div>
+        </div>
+        <div className='item-content'>
+          <div className='img-cover'>
+            <img src='https://via.placeholder.com/400x231' alt="" />
+          </div>
+          <div className='item-info'>
+            <div className='img-channel'>
+              <img src='https://via.placeholder.com/36x36' alt="" />
+            </div>
+            <div className='item-info-text'>
+              <h4 className='text-title'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident eum eos aperiam molestiae excepturi doloremque neque voluptatum error sed quasi.</h4>
+              <p className='text-channel'>King Soyer</p>
+              <p className='text-countWatch'> การดู 1.6 แสน ครั้ง 15 ชั่วโมงที่ผ่านมา</p>
+            </div>
+          </div>
+        </div>
+        <div className='item-content'>
+          <div className='img-cover'>
+            <img src='https://via.placeholder.com/400x231' alt="" />
+          </div>
+          <div className='item-info'>
+            <div className='img-channel'>
+              <img src='https://via.placeholder.com/36x36' alt="" />
+            </div>
+            <div className='item-info-text'>
+              <h4 className='text-title'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident eum eos aperiam molestiae excepturi doloremque neque voluptatum error sed quasi.</h4>
+              <p className='text-channel'>King Soyer</p>
+              <p className='text-countWatch'> การดู 1.6 แสน ครั้ง 15 ชั่วโมงที่ผ่านมา</p>
+            </div>
+          </div>
+        </div>
+        <div className='item-content'>
+          <div className='img-cover'>
+            <img src='https://via.placeholder.com/400x231' alt="" />
+          </div>
+          <div className='item-info'>
+            <div className='img-channel'>
+              <img src='https://via.placeholder.com/36x36' alt="" />
+            </div>
+            <div className='item-info-text'>
+              <h4 className='text-title'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident eum eos aperiam molestiae excepturi doloremque neque voluptatum error sed quasi.</h4>
+              <p className='text-channel'>King Soyer</p>
+              <p className='text-countWatch'> การดู 1.6 แสน ครั้ง 15 ชั่วโมงที่ผ่านมา</p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
